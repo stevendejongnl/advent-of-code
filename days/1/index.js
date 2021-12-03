a23160826d451af3d800c290e4031467aa3d6813
StevendeJong🍟 <steven@steven-dejong.nl>
2021-12-03 23:34:27 +0100

    Add part 2 of day 1

--- days/1/index.js
+++ days/1/index.js
@@ -1,17 +1,52 @@
 import data from './input.json'
 
-const input = data.input
+const measurements = data.input
 
-let previousMeasurement = null
-let totalLarger = 0
-input.forEach(measurement => {
-    if (measurement > previousMeasurement) {
-        totalLarger++
-    }
+function partOne(measurements) {
+    let previousMeasurement = null
+    let totalLarger = 0
+    measurements.forEach(measurement => {
+        if (parseInt(measurement) > previousMeasurement) {
+            totalLarger++
+        }
 
-    previousMeasurement = measurement
-})
+        previousMeasurement = parseInt(measurement)
+    })
 
-console.info('Result part one')
-console.log(totalLarger)
+    return totalLarger
+}
 
+function partTwo(measurements) {
+    let totalLarger = 0
+    let newMeasurements = []
+    measurements
+      .map(measurement => parseInt(measurement))
+      .forEach((measurement, index, array) => {
+        if (array[index - 1] && array[index] && array[index + 1]) {
+          let sum = array[index - 1] + array[index] + array[index + 1]
+            newMeasurements.push(sum)
+            return sum
+        }
+      })
+
+    newMeasurements
+      .reduce((previous, current) => {
+        if (previous < current) {
+          totalLarger++
+        }
+        return current
+      })
+
+    return totalLarger
+}
+
+function Parts(one, two) {
+    this.partOne = one
+    this.partTwo = two
+}
+
+const partOneResult = partOne(measurements)
+const partTwoResult = partTwo(measurements)
+const parts = new Parts(partOneResult, partTwoResult)
+
+console.table(parts)
