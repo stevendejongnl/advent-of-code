3a1cfcb37c87b619fb19585afa907d5bdbf8d927
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2022-12-07 11:43:22 +0100

    Add template directory

---
+++ 2022/__template__/day_[day].py
@@ -0,0 +1,15 @@
+def get_input(test=False):
+    input_file = f"input{'_test' if test else ''}.txt"
+
+    file = open(input_file, mode='r')
+
+    return file.read()
+
+def part_one(test=False):
+    file = get_input(test)
+    return file
+
+
+def part_two(test=False):
+    file = get_input(test)
+    return file
---
+++ 2022/__template__/test_day_[day].py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class TestDay[day](unittest.TestCase):
+    def test_part_one_example(self):
+        pass
+
+    def test_part_one(self):
+        pass
+
+    def test_part_two_example(self):
+        pass
+
+    def test_part_two(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
